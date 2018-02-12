bceec0afd0d20df02ab077a2ab05c140906c4345
PupiTooo <pupe.dupe@gmail.com>
2018-02-12 14:08:24 +0100

    update prospects adding

--- src/Smartsupp/Woodpecker/Woodpecker.php
+++ src/Smartsupp/Woodpecker/Woodpecker.php
@@ -27,12 +27,58 @@ class Woodpecker
 	/**
 	 * Get Prospects
 	 *
-	 * @return object
+	 * @return array
 	 */
 	public function getProspects()
 	{
 		$response = $this->request->get('prospects');
 		return $this->getJsonResponse($response);
+	}
+
+
+	/**
+	 * Add Prospect
+	 *
+	 * @return int|null
+	 */
+	public function addProspect($campaignId, $email)
+	{
+		$response = $this->addProspects($campaignId, [$email]);
+		return array_key_exists($email, $response) ? $response[$email] : null;
+	}
+
+
+	/**
+	 * Add Prospects
+	 *
+	 * @return array
+	 */
+	public function addProspects($campaignId, array $emails = [])
+	{
+		$prospects = [];
+		foreach ($emails as $email) {
+			$prospects[] = [
+				'email' => $email,
+				'status' => 'ACTIVE',
+			];
+		}
+		$data = [
+			'campaign' => [
+				'campaign_id' => $campaignId,
+			],
+			'update' => true,
+			'prospects' => $prospects,
+		];
+		$response = $this->request->post('add_prospects_campaign', $data);
+		$responseObject = $this->getJsonResponse($response);
+
+		$prospects = [];
+		if (isset($responseObject->prospects) && is_array($responseObject->prospects)) {
+			foreach ($responseObject->prospects as $prospect) {
+				$prospects[$prospect->email] = $prospect->id;
+			}
+		}
+		return $prospects;
 	}
 
 
